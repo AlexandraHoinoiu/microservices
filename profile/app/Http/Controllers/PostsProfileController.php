813537b03dfcc7d96a810577914f992b2408fad2
AlexandraHoinoiu <alexandrahoinoiu@gmail.com>
2021-06-27 23:38:44 +0300

    add new logic for posts pagination

--- profile/app/Http/Controllers/PostsProfileController.php
+++ profile/app/Http/Controllers/PostsProfileController.php
@@ -20,28 +20,17 @@ class PostsProfileController extends Controller
     public function getProfilePosts(Request $request): JsonResponse
     {
         try {
-            $limit = config('profile.limitPosts');
             $type = $request->get('type');
             $userId = $request->get('userId');
             $page = $request->get('page', 1);
             if ($page <= 0) {
                 $page = 1;
             }
-            $posts = $this->postModel->getProfilePosts($type, $userId);
-            if (!empty($posts)) {
-                usort($posts, function ($a, $b) {
-                    return $b['createdAt'] <=> $a['createdAt'];
-                });
-            }
-            $posts = array_chunk($posts, $limit);
-            $data = [];
-            if (isset($posts[$page - 1])) {
-                $data = $posts[$page - 1];
-            }
+            $posts = $this->postModel->getProfilePosts($type, $userId, $page);
             return response()->json([
                 "status" => 200,
                 "success" => true,
-                "data" => $data
+                "data" => $posts
             ]);
         } catch (\Exception $e) {
             return response()->json([
--- profile/app/Models/PostModel.php
+++ profile/app/Models/PostModel.php
@@ -8,12 +8,17 @@ class PostModel extends Neo4jModel
 {
     public string $label = 'Post';
 
-    public function getProfilePosts($type, $userId): array
+    public function getProfilePosts($type, $userId, $page): array
     {
+        $limit = config('profile.limitPosts');
+        $skip = $limit * ($page - 1);
         $posts = [];
         $result = $this->neo4jClient->run("MATCH (n:$type)<-[:CREATED_BY]-(p:$this->label)
             where id(n) = $userId
-            RETURN p, id(p) as id"
+            RETURN DISTINCT p, id(p) as id
+            order by p.createdAt DESC
+            skip $skip
+            limit $limit"
         );
         if ($result->count()) {
             foreach ($result as $post) {
